d21c74d00abaed640080169c9d6b263287d85697
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-12-07 18:33:49 +0100

    fixed use of deprecated ns

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
              :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "0.5.5-SNAPSHOT"]]
+                 [dda/dda-pallet "0.6.3"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
@@ -14,9 +14,9 @@
                    :dependencies
                     [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
                      [com.palletops/pallet "0.8.12" :classifier "tests"]
-                     [dda/dda-serverspec-crate "0.2.2-SNAPSHOT"]
+                     [dda/dda-serverspec-crate "0.4.0"]
                      [ch.qos.logback/logback-classic "1.2.3"]
-                     [org.slf4j/jcl-over-slf4j "1.8.0-alpha2"]]
+                     [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
                     :plugins
                     [[lein-sub "0.3.0"]]
                     :leiningen/reply
