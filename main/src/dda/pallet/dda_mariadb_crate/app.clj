9b01dc1b5c9c4fc766e5780a493d0154c93f54ae
jem <michael.jerger@meissa-gmbh.de>
2018-01-12 18:34:59 +0100

    refactored "fat-folder" & "fat-jar"

--- main/src/dda/pallet/dda_mariadb_crate/app.clj
+++ main/src/dda/pallet/dda_mariadb_crate/app.clj
@@ -18,9 +18,14 @@
   (:require
     [schema.core :as s]
     [dda.cm.group :as group]
+    [dda.pallet.commons.secret :as secret]
+    [dda.pallet.commons.existing :as existing]
+    [dda.pallet.commons.external-config :as ext-config]
     [dda.pallet.dda-config-crate.infra :as config-crate]
     [dda.pallet.dda-mariadb-crate.infra :as infra]
     [dda.pallet.dda-mariadb-crate.domain :as domain]))
+
+(def with-mariadb infra/with-mariadb)
 
 (def InfraResult infra/InfraResult)
 
@@ -28,14 +33,30 @@
   {:group-specific-config
    {s/Keyword InfraResult}})
 
-(def with-mariadb infra/with-mariadb)
+(s/defn ^:always-validate
+  load-targets :- existing/Targets
+  [file-name :- s/Str]
+  (existing/load-targets file-name))
 
-(s/defn ^:always-validate app-configuration :- AppConfig
-  [domain-config :- domain/DomainConfig
+(s/defn ^:always-validate
+  load-domain :- domain/DomainConfig
+  [file-name :- s/Str]
+  (ext-config/parse-config file-name))
+
+(s/defn ^:always-validate
+  app-configuration-resolved :- AppConfig
+  [domain-config :- domain/DomainConfigResolved
    & options]
   (let [{:keys [group-key] :or {group-key infra/facility}} options]
     {:group-specific-config
      {group-key (domain/infra-configuration domain-config)}}))
+
+(s/defn ^:always-validate
+  app-configuration :- AppConfig
+  [domain-config :- domain/DomainConfig
+   & options]
+  (let [resolved-domain-config (secret/resolve-secrets domain-config domain/DomainConfig)]
+    (apply app-configuration-resolved resolved-domain-config options)))
 
 (s/defn ^:always-validate mariadb-group-spec
  [app-config :- AppConfig]
--- project.clj
+++ project.clj
@@ -1,27 +1,40 @@
-(defproject dda/dda-mariadb-crate "0.2.3-SNAPSHOT"
+(defproject dda/dda-mariadb-crate "0.3.0-SNAPSHOT"
   :description "mariadb crate of dda-pallet project"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
              :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "0.6.3"]]
+                 [dda/dda-pallet "0.7.0-SNAPSHOT"]]
+  :source-paths ["main/src"]
+  :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
                         ["releases" :clojars]]
-  :profiles {:dev {:source-paths ["integration"]
-                   :resource-paths ["dev-resources"]
+  :profiles {:dev {:source-paths ["integration/src"
+                                  "test/src"
+                                  "uberjar/src"]
+                   :resource-paths ["integration/resources"
+                                    "test/resources"]
                    :dependencies
-                    [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
-                     [com.palletops/pallet "0.8.12" :classifier "tests"]
-                     [dda/dda-serverspec-crate "0.4.0"]
-                     [ch.qos.logback/logback-classic "1.2.3"]
-                     [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
-                    :plugins
-                    [[lein-sub "0.3.0"]]
-                    :leiningen/reply
-                    {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.22"]]
-                     :exclusions [commons-logging]}}}
-   :local-repo-classpath true
-   :classifiers {:tests {:source-paths ^:replace ["test" "integration"]
-                         :resource-paths ^:replace ["dev-resources"]}})
+                   [[org.clojure/test.check "0.10.0-alpha2"]
+                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
+                    [com.palletops/pallet "0.8.12" :classifier "tests"]
+                    [dda/dda-pallet-commons "0.7.1-SNAPSHOT" :classifier "tests"]
+                    [ch.qos.logback/logback-classic "1.2.3"]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
+                   :plugins
+                   [[lein-sub "0.3.0"]]
+                   :repl-options {:init-ns dda.pallet.dda-mariadb-crate.app.instantiate-aws}
+                   :leiningen/reply
+                   {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
+                    :exclusions [commons-logging]}}
+             :test {:test-paths ["test/src"]
+                    :resource-paths ["test/resources"]
+                    :dependencies [[com.palletops/pallet "0.8.12" :classifier "tests"]]}
+             :uberjar {:source-paths ["uberjar/src"]
+                       :resource-paths ["uberjar/resources"]
+                       :aot :all
+                       :main dda.pallet.dda-mariadb-crate.main
+                       :dependencies [[org.clojure/tools.cli "0.3.5"]]}}
+  :local-repo-classpath true)
