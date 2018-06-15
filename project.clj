8b6442862b7ade5a71e2d4384f67c4fc103419c1
jem <michael.jerger@meissa-gmbh.de>
2018-06-15 16:02:28 +0200

    use clojure 1.9

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
              :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "2.1.2"]]
+                 [dda/dda-pallet "2.2.0"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -18,8 +18,9 @@
                                     "test/resources"]
                    :dependencies
                    [[org.clojure/test.check "0.10.0-alpha2"]
-                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
-                    [com.palletops/pallet "0.8.12" :classifier "tests"]
+                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"
+                     :exclusions [com.palletops/pallet]]
+                    [dda/pallet "0.9.0" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
                     [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]
                    :plugins
@@ -27,13 +28,22 @@
                    :leiningen/reply
                    {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
                     :exclusions [commons-logging]}
-                   :repl-options {:init-ns dda.pallet.dda-mariadb-crate.app.instantiate-aws}}
+                   :repl-options {:init-ns dda.pallet.dda-mariadb-crate.app.instantiate-existing}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[com.palletops/pallet "0.8.12" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-mariadb-crate.main
                        :dependencies [[org.clojure/tools.cli "0.3.7"]]}}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["uberjar"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :local-repo-classpath true)
